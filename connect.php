46f0d779287e3865521ceafe2f805b542381c3b6
Your Name <annndutaw2020@gmail.com>
2023-11-11 14:30:48 +0300

    change

--- connect.php
+++ connect.php
@@ -1,10 +1,19 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
+// Database configuration
+$dbConfig = [
+    'host' => 'localhost',
+    'dbname' => 'student',
+    'user' => 'root',
+    'password' => '',
+];
+
 try {
-    $conn = new PDO("sqlsrv:server = tcp:studentmg-server.database.windows.net,1433; Database = studentmg-database", "studentmg-server-admin", "RORQ42Y6NFM17KE8$");
+    $conn = new PDO(
+        "mysql:host={$dbConfig['host']};dbname={$dbConfig['dbname']}",
+        $dbConfig['user'],
+        $dbConfig['password']
+    );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
