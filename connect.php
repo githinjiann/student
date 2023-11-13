9e35caf449dac253780603172062c6a72cf8bee9
Your Name <annndutaw2020@gmail.com>
2023-11-13 20:33:25 +0300

    ee

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:studentk-server.database.windows.net,1433; Database = studentk-database", "studentk-server-admin", "N2331OL26FWJ01ET$");
+    $conn = new PDO("sqlsrv:server = tcp:comrade-server.database.windows.net,1433; Database = comrade-database", "comrade-server-admin", "H71JS3F765V88EFU$");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
